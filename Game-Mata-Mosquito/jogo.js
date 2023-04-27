4a8795545ee11c2a49d9d397d52b72d93dc7cb38
Victor <victo.goncalves@hotmail.com>
2023-04-27 15:20:49 -0300

    new

---
+++ Game-Mata-Mosquito/jogo.js
@@ -0,0 +1,18 @@
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+
+    //console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+
+var posicaoX = Math.floor(Math.random() * largura)
+var posicaoY = Math.floor(Math.random() * altura)
+
+console.log(posicaoX, posicaoY)
