489f0ac636d33b819793c1104e7f97023a894cc5
Victor <victo.goncalves@hotmail.com>
2023-04-27 16:01:52 -0300

    new

--- Game-Mata-Mosquito/jogo.js
+++ Game-Mata-Mosquito/jogo.js
@@ -3,16 +3,49 @@ var altura = 0
 var largura = 0
 
 function ajustaTamanhoPalcoJogo(){
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+
+     altura = window.innerHeight
+     largura = window.innerWidth
 
     //console.log(altura, largura)
 }
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRondomica() {
 
-var posicaoX = Math.floor(Math.random() * largura)
-var posicaoY = Math.floor(Math.random() * altura)
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
-console.log(posicaoX, posicaoY)
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    // Cria o Elemento HTML
+
+    var mosca = document.createElement('img')
+    mosca.src = '../Game-Mata-Mosquito/imagens/mosca.png'
+    mosca.className = tamanhoAleatorio()
+    mosca.style.left = posicaoX + 'px'
+    mosca.style.top = posicaoY + 'px'
+    mosca.style.position = 'absolute'
+
+    document.body.appendChild(mosca)
+
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosca'
+        
+        case 1:
+            return 'mosca1'
+
+        case 2:
+            return 'mosca2'
+    }
+}
