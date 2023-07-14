5f5a41e7651c1bab0d573064a4cffbfa858a4c64
Victor <victo.goncalves@hotmail.com>
2023-07-14 18:01:49 -0300

    new

--- ECMAScript-Orcamento-Pessoal/app.js
+++ ECMAScript-Orcamento-Pessoal/app.js
@@ -7,7 +7,42 @@ class Despesas {
         this.descricao = descricao
         this.valor = valor
     }
+
+    validardados() {
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null ) {
+                return false
+            }
+        }
+        return true
+    }
 }
+
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa() {
 
@@ -29,6 +64,14 @@ function cadastrarDespesa() {
         valor.value
     )
 
-    console.log(despesa)
+    if (despesa.validardados()) {
+        //bd.gravar(despesa)
+        console.log('dados validos')
+    }else {
+        //dialogo de erro
+        console.log('dados Inválidos')
+    }
+
+    
 
 }
