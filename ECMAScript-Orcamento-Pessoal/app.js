1d63e88fe6a1cd1883c5f8d5080d99926cc12aab
Victor <victo.goncalves@hotmail.com>
2023-07-16 23:25:20 -0300

    new

--- ECMAScript-Orcamento-Pessoal/app.js
+++ ECMAScript-Orcamento-Pessoal/app.js
@@ -66,12 +66,24 @@ function cadastrarDespesa() {
 
     if (despesa.validardados()) {
         //bd.gravar(despesa)
-        console.log('dados validos')
+
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi Cadastrada com Sucesso'
+        document.getElementById('modal_btn').innerHTML = 'Voltar'
+        document.getElementById('modal_btn').className = 'btn btn-success'
+
+        //dialogo de sucesso
+        $('#modalRegistraDespesa').modal('show')
     }else {
+
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_titulo').innerHTML = 'Erro na Inclusão do Registro'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na Gravação, Verifique se todos os campos foram preenchidos corretamente'
+        document.getElementById('modal_btn').innerHTML = 'Voltar e Corrigir'
+        document.getElementById('modal_btn').className = 'btn btn-danger'
+
         //dialogo de erro
-        console.log('dados Inválidos')
+        $('#modalRegistraDespesa').modal('show')
     }
-
-    
-
 }
