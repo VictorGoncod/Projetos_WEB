d34deb078487b85b90245bdf564c8b5f7744b230
Victor <victo.goncalves@hotmail.com>
2024-10-08 16:55:51 -0300

    new

--- Aplicação Web CRUD/firebase-init.js
+++ Aplicação Web CRUD/firebase-init.js
@@ -1,3 +1,7 @@
+//import firebase from 'firebase/compat/app';
+//import 'firebase/compat/auth';
+//import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB1dT-VR0tOTB6Dtscp8yfV7YmWRm6L7HM",
     authDomain: "controle-de-gastos-cbaab.firebaseapp.com",
@@ -6,5 +10,5 @@ const firebaseConfig = {
     messagingSenderId: "906732256670",
     appId: "1:906732256670:web:7e680c9ab70b76eadfea8f"
   };
-  //firebase.initializeApp(firebaseConfig);
-  firebase.initializeApp(config);
+  //const app = initializeApp(firebaseConfig);
+  //firebase.initializeApp(config);
--- Aplicação Web CRUD/pages/home/home.js
+++ Aplicação Web CRUD/pages/home/home.js
@@ -6,6 +6,55 @@ function logout() {
     })
 }
 
+findTransactions();
+
+function findTransactions() {
+    firebase.firestore()
+        .collection('transactions')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => doc.data());
+            addTransactionsToScreen(transactions);
+        })
+}
+
+function addTransactionsToScreen(transactions) {
+    const orderedList = document.getElementById('transactions');
+
+    transactions.forEach(transaction => {
+        const li = document.createElement('li');
+        li.classList.add(transaction.type);
+
+        const date = document.createElement('p');
+        date.innerHTML = formatDate(transaction.date);
+        li.appendChild(date);
+
+        const money = document.createElement('p');
+        money.innerHTML = formatMoney(transaction.money);
+        li.appendChild(money);
+
+        const type = document.createElement('p');
+        type.innerHTML = transaction.transactionType;
+        li.appendChild(type);
+
+        if (transaction.description) {
+            const description = document.createElement('p');
+            description.innerHTML = transaction.description;
+            li.appendChild(description);
+        }
+
+        orderedList.appendChild(li);
+    });
+}
+
+function formatDate(date) {
+    return new Date(date).toLocaleDateString('pt-br');
+}
+
+function formatMoney(money) {
+    return `${money.currency} ${money.value.toFixed(2)}`
+}
+
 const fakeTransactions = [{
     type: 'expense',
     date: '2022-01-01',
@@ -21,8 +70,8 @@ const fakeTransactions = [{
         currency: 'R$',
         value: 609.00,
     },
-    transactionType: 'Salario'
-    decription: 'Empresa A'
+    transactionType: 'Salario',
+    description: 'Empresa A'
 },{
     type: 'expense',
     date: '2023-10-14',
@@ -30,6 +79,6 @@ const fakeTransactions = [{
         currency: 'R$',
         value: 609.00,
     },
-    transactionType: 'Salario'
-    decription: 'Empresa A'
+    transactionType: 'Salario',
+    description: 'Empresa A'
 }]
