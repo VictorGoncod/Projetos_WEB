20eeee451f01654c4c07651d37839f288bb3cced
Victor <victo.goncalves@hotmail.com>
2025-03-25 18:43:52 -0300

    new

--- Voo/js/btn-menu-mob.js
+++ Voo/js/btn-menu-mob.js
@@ -2,6 +2,7 @@
 let btnMenuMob = document.querySelector('#btn-menu-mob');
 let line1 = document.querySelector('.line-menumob-1');
 let line2 = document.querySelector('.line-menumob-2');
+let menuMobile = document.querySelector('#menu-mobile');
 
 btnMenuMob.addEventListener("click", () => {
     line1.classList.toggle('ativo1')
